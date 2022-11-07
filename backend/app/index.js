abff4fe974f9f112d321f6f360be78363ab44de6
SatrioBayu <sbayu875@gmail.com>
2022-11-07 14:30:25 +0700

    fix: content router.js moved into index.js

---
+++ backend/app/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const router = require("./router");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(router);
+
+module.exports = app;
